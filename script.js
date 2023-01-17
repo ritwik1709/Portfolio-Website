44e49c65a7482faacc8336cda44ee82a2f397a4b
Ritwik Sudhakar Tat <ritwikt17@gmail.com>
2023-01-18 00:26:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+//////////////// Sticky navigation //////////////////////
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+//////////////// Show scrollup //////////////////////
+function scrollUpfunc() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 680) scrollUp.classList.remove("hide");
+  else scrollUp.classList.add("hide");
+}
+window.addEventListener("scroll", scrollUpfunc);
+
+//////////////// SMOOTH scrolling //////////////////////
+document.querySelector(".main-nav-list").addEventListener("click",function(e){
+  e.preventDefault();
+  if(e.target.classList.contains('main-nav-link')){
+    const id=e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior:"smooth",
+    });
+  }
+  //Closing mobile nav
+  if(e.target.classList.contains("main-nav-link")){
+    document.querySelector(".header").classList.toggle("nav-open");
+  }
+});
+
+//Smooth scrolling of scroll up button
+const section=document.getElementById("home");
+document.querySelector(".scrollup").addEventListener("click",function(e){
+  e.preventDefault();
+  section.scrollIntoView({behavior:"smooth"})
+});
+
+
+//////////////// Mobile Navigation //////////////////////
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+////////////////Slider component //////////////////////
+
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
+
+//Background animation
